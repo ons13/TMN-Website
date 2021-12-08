be11a65090c433fc2cd0d78234d36bc76d4dbba0
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-12-08 13:57:06 +0100

    init

--- user/src/components/latestComp.js
+++ user/src/components/latestComp.js
@@ -1,27 +1,29 @@
-import {Text, Card, Image } from '@mantine/core';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Text, Card, Image } from "@mantine/core";
+import React from "react";
+import { Link } from "react-router-dom";
 
 function LatestNews(props) {
-    return (
-        <div >
-            
-        <Card style={{width:'400px', height:'300px'}} shadow="sm" padding="xl" component={Link} to={"/"+props.id}>
+  return (
+    <div>
+      <Card
+        style={{ width: "400px", height: "300px" }}
+        shadow="sm"
+        padding="xl"
+        component={Link}
+        to={"/" + props.id}
+      >
         <Card.Section>
-                <Image src={props.src} height={200} withPlaceholder />
+          <Image src={props.src} height={200} withPlaceholder />
         </Card.Section>
 
-        <Text style={{marginTop:'20px'}} weight={500} size="lg">
-            {props.title}
+        <Text style={{ marginTop: "20px" }} weight={500} size="lg">
+          {props.title}
         </Text>
 
-        <Text size="sm">
-            {props.description}
-         </Text>
-        </Card>
-           
-        </div>
-    );
+        <Text size="sm">{props.description}</Text>
+      </Card>
+    </div>
+  );
 }
 
 export default LatestNews;
