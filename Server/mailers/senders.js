26c63f2c1dcf27c12c9ed12ec777f023eabe5802
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-12-08 13:27:08 +0100

    init

--- Server/mailers/senders.js
+++ Server/mailers/senders.js
@@ -2,26 +2,25 @@ const { email } = require("./config");
 
 const welcomeSender = (recipient, name, code) => {
   console.log("Called in Sender");
-    email
+  email
     .send({
       template: "welcome",
       message: {
-        to: recipient,        
+        to: recipient,
       },
       locals: {
         name: name,
-        code: code
-      }
+        code: code,
+      },
     })
     .then(console.log)
     .catch(console.error);
 };
 
-
 /* const forgotPasswordSender = (name, code) => {
    
 }; */
 
-module.exports = {    
-    welcomeSender
+module.exports = {
+  welcomeSender,
 };
